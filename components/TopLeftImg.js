33c75613df33883ebdeaa840669dee97eda331bd
Marcel Dwi Astika <marceldwias@gmail.com>
2023-11-18 21:46:32 +0700

    Testing Deploy Image

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,10 @@
+import Image from "next/image";
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div>
+      <Image src="/top-left-img.png" width={400} height={400} alt="Left Decoration" />;
+    </div>
+  );
 };
 
 export default TopLeftImg;
