eb0b08f361fe34ed67aad109bf904d521dbc9d6f
Marcel Dwi Astika <marceldwias@gmail.com>
2023-11-20 02:05:04 +0700

    Homepage
    update: add animation homepage

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,10 @@
+import Image from "next/image";
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image src={"/final-avatar.png"} width={737} height={678} alt="My Photo" className="translate-z-0 w-full h-full mix-blend-luminosity" />
+    </div>
+  );
 };
 
 export default Avatar;
