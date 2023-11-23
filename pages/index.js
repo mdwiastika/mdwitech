dae2876ebb40367e11ad9ae79a02b1abf3d8ef31
Marcel Dwi Astika <marceldwias@gmail.com>
2023-11-23 19:15:25 +0700

    Contact page
    Add

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ import { easeInOut, motion } from "framer-motion";
 import { fadeIn } from "../variants";
 const Home = () => {
   return (
-    <div className="bg-primary/60 h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-      <div className="w-full h-full">
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div
           className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto
-        "
+      "
         >
           <motion.h1 className="h1" variants={fadeIn("down", 0.2)} initial="hidden" animate="show" exit="hidden">
             Forging Imagination <br /> Into <span className="text-accent">Digital Experiences</span>
@@ -28,8 +28,8 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="w-[1200px] h-full absolute right-0 bottom-0">
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+      <div className="hidden xl:block w-[1200px] h-full absolute right-0 bottom-0">
+        <motion.div variants={fadeIn("up", 0.2)} initial="hidden" animate="show" exit="hidden" transition={{ duration: 1, ease: "easeInOut" }} className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></motion.div>
         <ParticlesContainer />
         <motion.div className=" h-full w-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] mix-blend-luminosity" variants={fadeIn("up", 0.5)} initial="hidden" animate="show" exit="hidden" transition={{ duration: 1, ease: "easeInOut" }}>
           <Avatar />
